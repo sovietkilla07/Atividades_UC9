ed94ff7540e8b7fda97230db65d758af8db17645
sovietkilla07 <sovietmoderator109@gmail.com>
2023-01-27 17:59:46 -0300

    menu de barras quase funcionando

--- EPlayers/js/script.js
+++ EPlayers/js/script.js
@@ -3,3 +3,10 @@ function cadastrarNewsletter(){
     let email = document.getElementById("campo-email").value
     alert(email)
 }
+
+function mostrarMenu($event){
+
+    let menu = document.getElementById("menu")
+    menu.style.display = "flex"
+    event.preventDefault()
+}
