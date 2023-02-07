78375e3cf2528153e263af0edfa567c158427dec
sovietkilla07 <sovietmoderator109@gmail.com>
2023-02-07 20:14:00 -0300

    quase pronto

--- EPlayers/js/script.js
+++ EPlayers/js/script.js
@@ -1,8 +1,29 @@
 $(document).ready(function(){
 
+    let slideAtual = 1
+
     let listaSlides = ["banner-f1", "banner-cs", "banner-lol"]
 
+    setInterval(mudarSlide, 2000)
+
     function mudarSlide (){
+
+        //remover o slide anterior
+
+        
+        $("#carrossel").removeClass(listaSlides[slideAtual - 1])
+
+
+        //exibir slide atual
+
+        $("#carrossel").addClass(listaSlides[slideAtual])
+
+
+        //indicar slide atual
+        
+        console.log(" O slide atual é", slideAtual)
+
+        slideAtual ++
 
 
     }
